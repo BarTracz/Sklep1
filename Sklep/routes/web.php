b532df810d4f02fcab082759f72749c708fdcd7d
orlowski11 <pawel.orlowski@studenci.collegiumwitelona.pl>
2023-02-25 21:08:27 +0100

    v6.00

--- Sklep/routes/web.php
+++ Sklep/routes/web.php
@@ -25,6 +25,10 @@ Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function () {
 
     Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
 
+    Route::get('category', [App\Http\Controllers\Admin\CategoryController::class, 'index']);
+    Route::get('category/create', [App\Http\Controllers\Admin\CategoryController::class, 'create']);
+    Route::post('category', [App\Http\Controllers\Admin\CategoryController::class, 'store']);
+
     //Route::controller(App\Http\Controllers\Admin\SliderController::class)->group(function () {
     //    Route::get('sliders', 'index');
     //    Route::get('sliders/create', 'create');
