f92d2ec28e25c62bc67013a7c31f90b22b351370
orlowski11 <pawel.orlowski@studenci.collegiumwitelona.pl>
2023-03-17 13:08:31 +0100

    v28.00

--- Sklep/resources/views/livewire/frontend/product/view.blade.php
+++ Sklep/resources/views/livewire/frontend/product/view.blade.php
@@ -1,6 +1,11 @@
 <div>
 <div class="py-3 py-md-5">
         <div class="container">
+            @if (session()->has('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+            @endif
             <div class="row">
                 <div class="col-md-5 mt-3">
                     <div class="bg-white border">
@@ -13,15 +18,21 @@
                     <div class="product-view">
                         <h4 class="product-name">
                             {{ $product->name }}
-                            <label class="label-stock bg-success">In Stock</label>
                         </h4>
                         <hr>
                         <p class="product-path">
-                            Home / Category / Product / HP Laptop
+                            Home / {{ $product->category_name }} / {{ $product->name }}
                         </p>
                         <div>
                             <span class="selling-price">${{ $product->price }}</span>
                             <span class="original-price">$499</span>
+                        </div>
+                        <div>
+                            @if($product->quantity)
+                            <label class="btn-sm py-1 px-1 mt-2 text-white bg-success">In Stock</label>
+                            @else
+                            <label class="btn-sm py-1 px-1 mt-2 text-white bg-danger">Out of Stock</label>
+                            @endif
                         </div>
                         <div class="mt-2">
                             <div class="input-group">
@@ -32,7 +43,12 @@
                         </div>
                         <div class="mt-2">
                             <a href="" class="btn btn1"> <i class="fa fa-shopping-cart"></i> Add To Cart</a>
-                            <a href="" class="btn btn1"> <i class="fa fa-heart"></i> Add To Wishlist </a>
+                            <button type="button" wire:click="addToWishList({{ $product->id }})" class="btn btn1"> 
+                                <span wire:loading.remove>
+                                    <i class="fa fa-heart"></i> Add To Wishlist 
+                                </span>
+                                <span wire:loading wire:target="addToWishList">Adding...</span>
+                            </button>
                         </div>
                         <div class="mt-3">
                             <h5 class="mb-0">Parameters</h5>
